b28850c97aae0f4d50592e0a966f0fc7ef557692
qiyubrother <qiyubrother@outlook.com>
2019-11-01 18:27:11 +0800

    添加项目文件。

---
+++ ProcessMonitoring/FrmMain.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ProcessMonitoring
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMonitoring = new System.Windows.Forms.Button();
+            this.txtSearchKey = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxProcessIdType = new System.Windows.Forms.ComboBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvProcess = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgvThreads = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProcess)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThreads)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMonitoring
+            // 
+            this.btnMonitoring.Location = new System.Drawing.Point(457, 24);
+            this.btnMonitoring.Name = "btnMonitoring";
+            this.btnMonitoring.Size = new System.Drawing.Size(75, 23);
+            this.btnMonitoring.TabIndex = 0;
+            this.btnMonitoring.Text = "监控";
+            this.btnMonitoring.UseVisualStyleBackColor = true;
+            this.btnMonitoring.Click += new System.EventHandler(this.btnMonitoring_Click);
+            // 
+            // txtSearchKey
+            // 
+            this.txtSearchKey.Location = new System.Drawing.Point(200, 25);
+            this.txtSearchKey.Name = "txtSearchKey";
+            this.txtSearchKey.Size = new System.Drawing.Size(170, 21);
+            this.txtSearchKey.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "进程名",
+            "进程ID"});
+            this.comboBox1.Location = new System.Drawing.Point(14, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(103, 20);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBoxProcessIdType
+            // 
+            this.comboBoxProcessIdType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxProcessIdType.FormattingEnabled = true;
+            this.comboBoxProcessIdType.Items.AddRange(new object[] {
+            "十进制",
+            "十六进制"});
+            this.comboBoxProcessIdType.Location = new System.Drawing.Point(123, 26);
+            this.comboBoxProcessIdType.Name = "comboBoxProcessIdType";
+            this.comboBoxProcessIdType.Size = new System.Drawing.Size(71, 20);
+            this.comboBoxProcessIdType.TabIndex = 6;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(14, 55);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1140, 599);
+            this.splitContainer1.SplitterDistance = 299;
+            this.splitContainer1.SplitterWidth = 8;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvProcess);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1140, 299);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "进程信息";
+            // 
+            // dgvProcess
+            // 
+            this.dgvProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvProcess.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvProcess.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvProcess.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProcess.Location = new System.Drawing.Point(6, 20);
+            this.dgvProcess.Name = "dgvProcess";
+            this.dgvProcess.RowTemplate.Height = 23;
+            this.dgvProcess.Size = new System.Drawing.Size(1126, 273);
+            this.dgvProcess.TabIndex = 0;
+            this.dgvProcess.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProcess_CellEnter);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1126, 260);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(376, 24);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "查找";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1140, 292);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1132, 266);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "跟踪记录";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvThreads);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1132, 266);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "线程信息";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgvThreads
+            // 
+            this.dgvThreads.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvThreads.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvThreads.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThreads.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvThreads.Location = new System.Drawing.Point(3, 3);
+            this.dgvThreads.Name = "dgvThreads";
+            this.dgvThreads.RowTemplate.Height = 23;
+            this.dgvThreads.Size = new System.Drawing.Size(1126, 260);
+            this.dgvThreads.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 669);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.comboBoxProcessIdType);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtSearchKey);
+            this.Controls.Add(this.btnMonitoring);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "进程监控程序";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProcess)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThreads)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMonitoring;
+        private System.Windows.Forms.TextBox txtSearchKey;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxProcessIdType;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvProcess;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dgvThreads;
+    }
+}
+
---
+++ ProcessMonitoring/FrmMain.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProcessMonitoring
+{
+    public partial class FrmMain : Form
+    {
+        DataTable dtProcess = new DataTable();
+        DataTable dtThreads = new DataTable();
+        public FrmMain()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBoxProcessIdType.SelectedIndex = 0;
+            dtProcess.Columns.Add("Process ID (Hex)", typeof(string));
+            dtProcess.Columns.Add("Process ID (Dec)", typeof(string));
+            dtProcess.Columns.Add("Process Name", typeof(string));
+            dtProcess.Columns.Add("Action", typeof(string));
+            dtProcess.Columns.Add("File Name", typeof(string));
+
+            dgvProcess.DataSource = new BindingSource(dtProcess, string.Empty);
+            dgvProcess.AllowUserToAddRows = false;
+            dgvProcess.AllowUserToDeleteRows = false;
+            dgvProcess.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvProcess.ReadOnly = true;
+            dgvProcess.Columns[0].Width = 150;
+            dgvProcess.Columns[1].Width = 150;
+            dgvProcess.Columns[2].Width = 150;
+            dgvProcess.Columns[3].Width = 100;
+            dgvProcess.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            dtThreads.Columns.Add("Thread ID (Hex)", typeof(string));
+            dtThreads.Columns.Add("Thread ID (Dec)", typeof(string));
+            dtThreads.Columns.Add("State", typeof(string));
+
+            dgvThreads.DataSource = new BindingSource(dtThreads, string.Empty);
+            dgvThreads.AllowUserToAddRows = false;
+            dgvThreads.AllowUserToDeleteRows = false;
+            dgvThreads.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvThreads.ReadOnly = true;
+
+            dgvProcess.Columns[0].Width = 150;
+            dgvProcess.Columns[1].Width = 150;
+            dgvProcess.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void btnMonitoring_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                // 通过进程名查找
+                var processList = Process.GetProcessesByName(txtSearchKey.Text);
+                dtProcess.Clear();
+                foreach (var process in processList)
+                {
+                    var dr = dtProcess.NewRow();
+                    dr[0] = Convert.ToString(process.Id, 16).PadLeft(8, '0').ToUpper();
+                    dr[1] = Convert.ToString(process.Id, 10);
+                    dr[2] = process.ProcessName;
+                    dr[3] = string.Empty;
+                    dr[4] = process.MainModule.FileName;
+                    dtProcess.Rows.Add(dr);
+                }
+                dtProcess.AcceptChanges();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                int processId = 0;
+
+                // 通过进程ID查找
+                if (comboBoxProcessIdType.SelectedIndex == 0)
+                {
+                    // 十进制
+                    processId = Convert.ToInt32(txtSearchKey.Text);
+                }
+                else if (comboBoxProcessIdType.SelectedIndex == 1)
+                {
+                    // 十六进制
+                    processId = Convert.ToInt32(txtSearchKey.Text, 16);
+                }
+
+                var process = Process.GetProcessById(processId);
+                dtProcess.Clear();
+
+                var dr = dtProcess.NewRow();
+                dr[0] = Convert.ToString(process.Id, 16).PadLeft(8, '0').ToUpper();
+                dr[1] = Convert.ToString(process.Id, 10);
+                dr[2] = process.ProcessName;
+                dr[3] = string.Empty;
+                dr[4] = process.MainModule.FileName;
+                dtProcess.Rows.Add(dr);
+
+                dtProcess.AcceptChanges();
+            }
+            else
+            {
+                // 未定义
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                comboBoxProcessIdType.Visible = false;
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                comboBoxProcessIdType.Visible = true;
+            }
+        }
+
+        private void dgvProcess_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvProcess.CurrentCell == null)
+            {
+                dtThreads.Clear();
+                dtThreads.AcceptChanges();
+            }
+            else
+            {
+                var process = Process.GetProcessById(Convert.ToInt32(dgvProcess.CurrentCell.OwningRow.Cells[1].Value));
+                dtThreads.Clear();
+
+                foreach (ProcessThread thread in process.Threads)
+                {
+                    var dr = dtThreads.NewRow();
+                    dr[0] = Convert.ToString(thread.Id, 16).PadLeft(8, '0').ToUpper();
+                    dr[1] = Convert.ToString(thread.Id, 10);
+                    dr[2] = thread.ThreadState;
+                    dtThreads.Rows.Add(dr);
+                }
+                dtThreads.AcceptChanges();
+            }
+        }
+    }
+}
